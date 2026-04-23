129911cc27d10af8c5b43fe9ba61a482fa8ffad2
rafael-adrianosilva <rafael.a.silva64@aluno.senai.br>
2026-04-23 14:02:49 -0300

    debug error 2

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./database'); // Agora exporta o Firestore
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const app = express();
